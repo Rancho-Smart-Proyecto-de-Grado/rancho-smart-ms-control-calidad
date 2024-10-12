953f3b1eb75135921bfcb887394b5bf943ba7e2e
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:05:20 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ src/main/java/com/rancho_smart/ms_control_calidad/MsControlCalidadApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_control_calidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsControlCalidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsControlCalidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/ms_control_calidad/MsControlCalidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_control_calidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsControlCalidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
